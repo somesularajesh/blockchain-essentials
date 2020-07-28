d2132d7b34a5f0fcff0958b526dfc147b1903403
somesularajesh <67363551+somesularajesh@users.noreply.github.com>
2020-07-28 21:39:48 +0530

    reportday4

---
+++ bcreportday4.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.4.21 < 0.6.12;
+
+contract Report_Card{
+    
+    string public name;
+    uint public rollno;
+    uint public batch;
+    string public status;
+    uint  s1;
+    uint  s2;
+    uint  s3;
+    uint  s4;
+    
+    constructor(string newName,uint newRollno,uint newBatch,uint m1,uint m2,uint m3,uint m4)public{
+        name = newName;
+        rollno = newRollno;
+        batch = newBatch;
+        
+        s1 = m1;
+        s2 = m2;
+        s3 = m3;
+        s4 = m4;
+       
+     
+        if(s1>35 && s2>35 && s3>35 && s4>35 ){
+            status = "PASS";
+        }
+        else {
+            status = "FAILED because u failed in one or more subects so ur statsus if failed";
+        }
+        
+    }
+    
+    function getDetails()public view returns(string,uint,uint,string){
+       
+        return(name,rollno,batch,status);
+        
+    } 
+    
+}
